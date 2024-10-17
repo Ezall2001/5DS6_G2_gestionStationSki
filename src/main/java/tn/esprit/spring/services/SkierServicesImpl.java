d3ecc693e66af55a09fead38ef7443e1d1ccc56f
Ezall2001 <armen.bakir.e@gmail.com>
2024-10-17 07:13:20 +0000

    fix possible null ptr bug

--- src/main/java/tn/esprit/spring/services/SkierServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/SkierServicesImpl.java
@@ -80,7 +80,9 @@ public class SkierServicesImpl implements ISkierServices {
     @Override
     public Skier assignSkierToPiste(Long numSkieur, Long numPiste) {
         Skier skier = skierRepository.findById(numSkieur).orElse(null);
+	if(skier == null) return null
         Piste piste = pisteRepository.findById(numPiste).orElse(null);
+	
         try {
             skier.getPistes().add(piste);
         } catch (NullPointerException exception) {
