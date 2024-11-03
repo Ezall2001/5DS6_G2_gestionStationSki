6747994c478c7249f64747da8e03a6acf88e1040
Ahlemmhm <ahlemmhamdi294@gmail.com>
2024-11-03 19:57:18 +0100

    fixed instructor service

--- src/test/java/tn/esprit/spring/services/InstructorServicesImplTest.java
+++ src/test/java/tn/esprit/spring/services/InstructorServicesImplTest.java
@@ -176,13 +176,14 @@ class InstructorServicesImplTest {
     void testAddInstructorAndAssignToCourse_courseNotFound() {
         // Arrange
         when(courseRepository.findById(2L)).thenReturn(Optional.empty());
-
+	when(instructorRepository.save(any(Instructor.class))).thenReturn(instructor);
+										   
         // Act
         Instructor savedInstructor = instructorServices.addInstructorAndAssignToCourse(instructor, 2L);
 
         // Assert
         assertNotNull(savedInstructor);
-        assertTrue(savedInstructor.getCourses().isEmpty());
+        assertTrue(instructor.getCourses().isEmpty());
         verify(instructorRepository, times(1)).save(instructor);
         verify(courseRepository, times(1)).findById(2L);
     }
